c1332a0ac68a282ada522d6b9f3b548f297e8605
Nora Delgado <noradelgadobusot@gmail.com>
2025-04-29 22:46:01 -0400

    Update health_stats.R

--- R/health_stats.R
+++ R/health_stats.R
@@ -19,5 +19,23 @@ setwd("C:/Users/norae/Downloads")
 
 # Install package
 install("HealthStats")
+library(HealthStats)
+
+# Check if the help page appears
+?clean_wellness_data
 
 # Test package
+clean_data <- clean_wellness_data("C:/Users/norae/Downloads/wellness_data.csv")
+head(clean_data)
+
+# Put project under Git
+usethis::use_git_config(user.name = "Nora Delgado",
+                        user.email = "noradelgadobusot@gmail.com")
+use_git()
+
+# Create GitHub repo
+create_github_token()
+edit_r_environ()
+use_github(protocol = "https", private = FALSE)
+
+
